c185e4af3743b178d0341ec0dfaa3901c79e0595
Lastor-Chen <lastoraya@gmail.com>
2021-09-16 17:04:15 +0800

    [加入] vite build manual chunks 處理及大包 gzip 壓縮

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 import { defineConfig } from 'vite'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
-  base: '/'
+  base: '/',
+  plugins: [viteCompression()], // 太大的包進行 gzip 壓縮
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) { // 將 node_modules 依套件名拆小包
+          if (id.includes('node_modules')) {
+            const libName = id.split('node_modules/')[1].split('/')[0]
+            return libName
+          }
+        },
+      },
+    },
+  },
 })
